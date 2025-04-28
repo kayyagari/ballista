14a0ad9211a77bcff5682d73dbd4c1e50d68702c
onlybond <36098799+onlybond@users.noreply.github.com>
2025-04-29 00:21:46 +0530

    moved from antd to tailwind and shadcn.

---
+++ src/hooks/use-debounce.ts
@@ -0,0 +1,15 @@
+// debounce hook
+import { useEffect, useState } from "react";
+
+export function useDebounce<T>(value: T, delay?: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+  return debouncedValue;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Dashbaord from "./components/Dashbaord";
+import ConnectionForm from "./components/ConnectionForm";
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Dashbaord />}>
+          <Route index path="" element={<ConnectionForm />} />
+          <Route path=":slug" element={<ConnectionForm />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
