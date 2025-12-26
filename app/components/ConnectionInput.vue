d363dae84b3e9401a3bd85007926c37a91c2d801
Kaur Palang <kaur.palang@brightcodecompany.com>
2025-12-26 13:34:34 +0200

    Add element ID attributes to ConnectionInput
    Signed-off-by: Kaur Palang <kaur.palang@brightcodecompany.com>

--- app/components/ConnectionInput.vue
+++ app/components/ConnectionInput.vue
@@ -5,11 +5,14 @@ defineProps<{
   label?: string
   type: string
 }>()
+
+const inputId = useId()
 </script>
 
 <template>
-  <label v-if="label" class="select-none font-bold">{{ label }}</label>
+  <label :for="inputId" v-if="label" class="select-none font-bold">{{ label }}</label>
   <input
+    :id="inputId"
     :type="type"
     class="col-start-2 col-span-2 border-2 border-teal-500 rounded-lg px-2 py-1"
     v-model="model"
